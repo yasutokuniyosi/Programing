d00de5c8fc856c34c4889d0e0bca71207f5a2ce7
yasutokuniyosi <138412658+yasutokuniyosi@users.noreply.github.com>
2024-02-02 09:45:37 +0900

    完成

--- Game/DriveAndAvoid/Scene/TitleScene.h
+++ Game/DriveAndAvoid/Scene/TitleScene.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "SceneBase.h"
+
+class TitleScene : public SceneBase
+{
+private:
+
+private:
+	int background_image;      //背景画像
+	int menu_image;            //メニュー画像
+	int cursor_image;          //カーソル画像
+	int menu_cursor;           //メニューカーソル画像
+
+public:
+	TitleScene();
+	virtual ~TitleScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
+};
